396cc2e8925f40da50705ed9e3914f3b85f6c2ec
Dan Macumber <dan.macumber@gmail.com>
2016-02-24 23:10:30 -0700

    Update scripts

--- data/clean_common.rb
+++ data/clean_common.rb
@@ -1,165 +1,187 @@
-def get_building_schema
-  result = nil
-  File.open(File.dirname(__FILE__) + "/../building_properties.json") do |f|
-    result = JSON.parse(f.read)
-  end
-  return result
-end
+class Cleaner
 
-def get_taxlot_schema
-  result = nil
-  File.open(File.dirname(__FILE__) + "/../taxlot_properties.json") do |f|
-    result = JSON.parse(f.read)
-  end
-  return result
-end
-
-def get_district_system_schema
-  result = nil
-  File.open(File.dirname(__FILE__) + "/../district_system_properties.json") do |f|
-    result = JSON.parse(f.read)
-  end
-  return result
-end
-
-def get_region_schema
-  result = nil
-  File.open(File.dirname(__FILE__) + "/../region_properties.json") do |f|
-    result = JSON.parse(f.read)
-  end
-  return result
-end
-
-def remove_nil_values(data)
-  data.keys.each do |key|
-    if data[key].nil?
-      data.delete(key)
+  def get_building_schema
+    result = nil
+    File.open(File.dirname(__FILE__) + "/../building_properties.json") do |f|
+      result = JSON.parse(f.read)
     end
+    return result
   end
-end
 
-def remove_unknown_keys(data, schema)
-  data.keys.each do |key|
-    if schema['properties'][key].nil?
-      data.delete(key)
+  def get_taxlot_schema
+    result = nil
+    File.open(File.dirname(__FILE__) + "/../taxlot_properties.json") do |f|
+      result = JSON.parse(f.read)
     end
+    return result
   end
-end
 
-def infer_geometry(data)
-end
-
-def infer_space_type(data)
-end
-
-def clean_buildings(city)
-
-  all_errors = {}
-  
-  schema = get_building_schema
-  
-  Dir.glob("#{city}_bldg_footprints_*.geojson").each do |p|
-
-    # skip already cleaned
-    next if /\.clean\.geojson$/.match(p)
-    
-    #next if !/08031004103/.match(p)
-    
-    geojson = nil
-    File.open(p, 'r') do |f|
-      geojson = JSON.parse(f.read)
+  def get_district_system_schema
+    result = nil
+    File.open(File.dirname(__FILE__) + "/../district_system_properties.json") do |f|
+      result = JSON.parse(f.read)
     end
-    
-    # loop over features
-    geojson['features'].each do |feature|
-      all_errors[p] = []
-      
-      begin
-        data = feature['properties']
-        
-        data['type'] = 'Building'
-        
-        # clean
-        infer_geometry(data)
-        infer_space_type(data)
-        remove_nil_values(data)
-        remove_unknown_keys(data, schema)
-      
-        # validate
-        errors = JSON::Validator.fully_validate(schema, data, :errors_as_objects => true)
-        all_errors[p].concat(errors)
-      rescue
-        all_errors[p] << "Removing feature:\n#{data}"
-        feature.clear
+    return result
+  end
+
+  def get_region_schema
+    result = nil
+    File.open(File.dirname(__FILE__) + "/../region_properties.json") do |f|
+      result = JSON.parse(f.read)
+    end
+    return result
+  end
+
+  def remove_nil_values(data)
+    data.keys.each do |key|
+      if data[key].nil?
+        data.delete(key)
       end
     end
-    
-    geojson['features'].delete_if {|feature| feature.keys.empty?}
-    
-    File.open(p.gsub('.geojson', '.clean.geojson'), 'w') do |f|
-      #f << JSON.generate(geojson)
-      f << JSON.pretty_generate(geojson)
-    end
-
-    break
   end
 
-  File.open("#{city}_errors.json", 'w') do |f|
-    f << JSON.generate(all_errors)
-  end
-end
-
-def clean_taxlots(city)
-
-  all_errors = {}
-  
-  schema = get_taxlot_schema
-  
-  Dir.glob("#{city}_land_use_*.geojson").each do |p|
-
-    # skip already cleaned
-    next if /\.clean\.geojson$/.match(p)
-    
-    #next if !/08031004103/.match(p)
-    
-    geojson = nil
-    File.open(p, 'r') do |f|
-      geojson = JSON.parse(f.read)
-    end
-    
-    # loop over features
-    geojson['features'].each do |feature|
-      all_errors[p] = []
-      
-      begin
-        data = feature['properties']
-        
-        data['type'] = 'Taxlot'
-        
-        # clean
-        remove_nil_values(data)
-        remove_unknown_keys(data, schema)
-      
-        # validate
-        errors = JSON::Validator.fully_validate(schema, data, :errors_as_objects => true)
-        all_errors[p].concat(errors)
-      rescue
-        all_errors[p] << "Removing feature:\n#{data}"
-        feature.clear
+  def remove_unknown_keys(data, schema)
+    data.keys.each do |key|
+      if schema['properties'][key].nil?
+        data.delete(key)
       end
     end
+  end
+
+  # override in derived class
+  
+  # return a string that identifies this cleaner
+  def name()
+    return ""
+  end
+  
+  # return a string that will match files to clean in glob
+  def file_pattern()
+    return ""
+  end
+
+  def clean_building(data, schema)
+    # clean
+    remove_nil_values(data)
+    remove_unknown_keys(data, schema)
+  
+    # validate
+    errors = JSON::Validator.fully_validate(schema, data, :errors_as_objects => true)
+    return errors
+  end
+
+  def clean_taxlot(data, schema)
+    # clean
+    remove_nil_values(data)
+    remove_unknown_keys(data, schema)
+  
+    # validate
+    errors = JSON::Validator.fully_validate(schema, data, :errors_as_objects => true)
+    return errors
+  end
+  
+  def clean_region(data, schema)
+    # clean
+    remove_nil_values(data)
+    remove_unknown_keys(data, schema)
+  
+    # validate
+    errors = JSON::Validator.fully_validate(schema, data, :errors_as_objects => true)
+    return errors
+  end
+  
+  def clean
+    all_errors = {}
     
-    geojson['features'].delete_if {|feature| feature.keys.empty?}
+    building_schema = get_building_schema
+    taxlot_schema = get_taxlot_schema
+    region_schema = get_region_schema
+    pattern = file_pattern
     
-    File.open(p.gsub('.geojson', '.clean.geojson'), 'w') do |f|
-      #f << JSON.generate(geojson)
-      f << JSON.pretty_generate(geojson)
+    Dir.glob(pattern).each do |p|
+      
+      # enforce .geojson extension
+      next unless /\.geojson$/.match(p)
+      
+      # skip already cleaned
+      next if /\.clean\.geojson$/.match(p)
+      
+      #next unless /08031004103/.match(p)
+      
+      puts "Processing #{p}"
+      
+      geojson = nil
+      File.open(p, 'r') do |f|
+        geojson = JSON.parse(f.read)
+      end
+      
+      # loop over features in original and sort keys
+      geojson['features'].each do |feature|
+        data = feature['properties']
+        feature['properties'] = Hash[data.sort]
+      end
+
+      File.open(p, 'w') do |f|
+        #f << JSON.generate(geojson)
+        f << JSON.pretty_generate(geojson)
+      end
+      
+      all_errors[p] = []
+      
+      # loop over features
+      geojson['features'].each do |feature|
+        all_errors[p] << []
+        
+        begin
+          data = feature['properties']
+          type = data['type']
+          
+          errors = []
+          if /building/i.match(type)
+            errors = clean_building(data, building_schema)
+          elsif /taxlot/i.match(type)
+            errors = clean_taxlot(data, taxlot_schema)
+          elsif /region/i.match(type)
+            errors = clean_region(data, region_schema)
+          else 
+            raise("Unknown type: '#{type}'")
+          end
+          
+          if !errors.empty?
+            all_errors[p][-1] << "Validation failed"
+            all_errors[p][-1].concat(errors)
+            all_errors[p][-1] << "Removing feature: "
+            all_errors[p][-1] << data.clone
+            feature.clear
+          else
+            feature['properties'] = Hash[data.sort]
+          end
+          
+        rescue Exception => e  
+          all_errors[p][-1] << "Error '#{e.message}' occurred: "
+          all_errors[p][-1] << "#{e.backtrace}"
+          all_errors[p][-1] << "Removing feature: "
+          all_errors[p][-1] << data.clone
+          feature.clear
+        end
+        
+        if all_errors[p][-1].empty?
+          all_errors[p].pop
+        end
+      end
+      
+      geojson['features'].delete_if {|feature| feature.keys.empty?}
+      
+      File.open(p.gsub('.geojson', '.clean.geojson'), 'w') do |f|
+        #f << JSON.generate(geojson)
+        f << JSON.pretty_generate(geojson)
+      end
+      
     end
 
-    break
+    File.open("#{name}_errors.json", 'w') do |f|
+      f << JSON.pretty_generate(all_errors)
+    end
   end
-
-  File.open("#{city}_errors.json", 'w') do |f|
-    f << JSON.generate(all_errors)
-  end
+  
 end
---
+++ data/clean_san_francisco.rb
@@ -0,0 +1,318 @@
+require 'rubygems'
+require 'json-schema'
+require_relative 'clean_common.rb'
+
+class SanFranciscoCleaner < Cleaner
+
+  def name()
+    return "San Francisco"
+  end
+  
+  # return a string that will match files to clean in glob
+  def file_pattern()
+    return "US_CA_*.geojson"
+  end
+  
+  def infer_geometry(data)
+    errors = []  
+      
+    total_height = nil
+    assumed_floor_to_floor_height = 3.65 # 12 ft
+
+    average_roof_height = data['average_roof_height']
+    average_roof_height_source = data['average_roof_height_source']
+    floor_area = data['floor_area']
+    floor_area_source = data['floor_area_source']
+    footprint_area = data['footprint_area']
+    footprint_area_source = data['footprint_area_source']    
+    maximum_roof_height = data['maximum_roof_height']
+    maximum_roof_height_source = data['maximum_roof_height_source']
+    minimum_roof_height = data['minimum_roof_height']
+    minimum_roof_height_source = data['minimum_roof_height_source']
+    number_of_stories = data['number_of_stories']
+    number_of_stories_source = data['number_of_stories_source']
+    number_of_stories_above_ground = data['number_of_stories_above_ground']
+    number_of_stories_above_ground_source = data['number_of_stories_above_ground_source']
+    number_of_stories_below_ground = data['number_of_stories_below_ground']
+    number_of_stories_below_ground_source = data['number_of_stories_below_ground_source']
+    roof_elevation = data['roof_elevation']
+    roof_elevation_source = data['roof_elevation_source']
+    surface_elevation = data['surface_elevation']
+    surface_elevation_source = data['surface_elevation_source']
+    
+    if number_of_stories == 0
+      number_of_stories = nil
+    end
+    
+    if surface_elevation && roof_elevation
+      total_height = roof_elevation.to_f - surface_elevation.to_f
+    elsif average_roof_height
+      total_height = average_roof_height.to_f
+    elsif minimum_roof_height && maximum_roof_height
+      total_height = (minimum_roof_height.to_f + maximum_roof_height.to_f) / 2.0
+    end
+    
+    if number_of_stories.nil?
+
+      num_floors_height = nil
+      if total_height
+        num_floors_height = total_height / assumed_floor_to_floor_height  
+      end
+    
+      num_floors_area = nil
+      if floor_area && footprint_area
+        num_floors_area = floor_area.to_f / footprint_area.to_f
+      end
+    
+      if num_floors_height && num_floors_area
+        number_of_stories = num_floors_area.round # prefer the area based weighting as height based metric does not include basements
+        number_of_stories_source = "Inferred"
+      elsif num_floors_height
+        number_of_stories = num_floors_height.round
+        number_of_stories_source = "Inferred"
+      elsif num_floors_area
+        number_of_stories = num_floors_area.round
+        number_of_stories_source = "Inferred"
+      else
+        number_of_stories = 1
+        number_of_stories_source = "Inferred"
+      end
+      
+      total_height = number_of_stories * assumed_floor_to_floor_height
+    end
+    
+    # check that our resulting floor area is not too much larger than the reported area
+    if footprint_area && floor_area
+      resulting_area = footprint_area * number_of_stories
+      if resulting_area > 1.2 * floor_area
+        new_result = [1, number_of_stories - 1].max
+        new_resulting_area = footprint_area * new_result
+        if (floor_area-new_resulting_area).abs < (floor_area-resulting_area).abs
+          number_of_stories = new_result
+          number_of_stories_source = "Inferred"
+          
+          total_height = number_of_stories * assumed_floor_to_floor_height
+        end
+      end
+    end
+
+    # now try to figure out number of above and below ground surfaces    
+    if number_of_stories > 1
+      floor_to_floor_height_no_basement = total_height / number_of_stories
+      floor_to_floor_height_with_basement = total_height / (number_of_stories - 1)
+      if (floor_to_floor_height_no_basement - assumed_floor_to_floor_height).abs < (floor_to_floor_height_with_basement - assumed_floor_to_floor_height).abs
+        number_of_stories_above_ground = number_of_stories
+        number_of_stories_above_ground_source = "Inferred"
+        number_of_stories_below_ground = 0
+        number_of_stories_below_ground_source = "Inferred"
+      else
+        number_of_stories_above_ground = number_of_stories-1
+        number_of_stories_above_ground_source = "Inferred"
+        number_of_stories_below_ground = 1
+        number_of_stories_below_ground_source = "Inferred"
+      end
+    else
+      number_of_stories_above_ground = number_of_stories
+      number_of_stories_above_ground_source = "Inferred"
+      number_of_stories_below_ground = 0
+      number_of_stories_below_ground_source = "Inferred"
+    end
+    
+    if roof_elevation.nil? || surface_elevation.nil?
+      surface_elevation = -number_of_stories_below_ground * assumed_floor_to_floor_height
+      surface_elevation_source = "Inferred"
+      roof_elevation = number_of_stories_above_ground * assumed_floor_to_floor_height
+      roof_elevation_source = "Inferred"
+    end
+    
+    if floor_area.nil?
+      floor_area = footprint_area * number_of_stories
+      floor_area_source = "Inferred" 
+    end
+    
+     data['average_roof_height'] = average_roof_height
+     data['average_roof_height_source'] = average_roof_height_source
+     data['floor_area'] = floor_area
+     data['floor_area_source'] = floor_area_source
+     data['footprint_area'] = footprint_area
+     data['footprint_area_source'] = footprint_area_source    
+     data['maximum_roof_height'] = maximum_roof_height
+     data['maximum_roof_height_source'] = maximum_roof_height_source
+     data['minimum_roof_height'] = minimum_roof_height
+     data['minimum_roof_height_source'] = minimum_roof_height_source
+     data['number_of_stories'] = number_of_stories
+     data['number_of_stories_source'] = number_of_stories_source
+     data['number_of_stories_above_ground'] = number_of_stories_above_ground
+     data['number_of_stories_above_ground_source'] = number_of_stories_above_ground_source
+     data['number_of_stories_below_ground'] = number_of_stories_below_ground
+     data['number_of_stories_below_ground_source'] = number_of_stories_below_ground_source
+     data['roof_elevation'] = roof_elevation
+     data['roof_elevation_source'] = roof_elevation_source
+     data['surface_elevation'] = surface_elevation
+     data['surface_elevation_source'] = surface_elevation_source
+
+    return errors
+  end
+
+  def infer_space_type(data)
+    errors = []
+    
+    zoning = data['zoning']
+    zoning_source = data['zoning_source']
+    floor_area = data['floor_area']
+    floor_area_source = data['floor_area_source']
+    number_of_stories = data['number_of_stories']
+    number_of_stories_source = data['number_of_stories_source']
+    space_type = data['space_type']
+    space_type_source = data['space_type_source']
+    
+    if zoning.nil?
+      zoning = "Vacant"
+      zoning_source = "Inferred"
+    end
+    
+    if space_type.nil?
+      if zoning == "Vacant"
+        space_type = "Vacant"
+        space_type_source = "Inferred"
+      elsif zoning == "Mixed"
+        if floor_area < 300
+          space_type = "Single-Family"
+          space_type_source = "Inferred"
+        elsif floor_area < 500
+          space_type = "Multifamily (2 to 4 units)"
+          space_type_source = "Inferred"
+        else
+          space_type = "Multifamily (5 or more units)"
+          space_type_source = "Inferred"
+        end
+      elsif zoning == "Residential"
+        if floor_area < 300
+          space_type = "Single-Family"
+          space_type_source = "Inferred"
+        elsif floor_area < 500
+          space_type = "Multifamily (2 to 4 units)"
+          space_type_source = "Inferred"
+        else
+          space_type = "Multifamily (5 or more units)"
+          space_type_source = "Inferred"
+        end
+      elsif zoning == "Commercial"
+        space_type = "Office"
+        space_type_source = "Inferred"
+      elsif zoning == "OpenSpace"
+        space_type = "Vacant"        
+        space_type_source = "Inferred"
+      end
+    end
+    
+    data['zoning'] = zoning
+    data['zoning_source'] = zoning_source
+    data['space_type'] = space_type
+    data['space_type_source'] = space_type_source
+    
+    return errors
+  end
+  
+  def clean_building(data, schema)
+    errors = []
+    
+    if data['type'] == 'building'
+      data['type'] = 'Building'
+    end
+    
+    if data['source_id'].class == Fixnum
+      data['source_id'] = data['source_id'].to_s
+    end
+    
+    if data['zoning'] == "VACANT"      
+      data['zoning'] = "Vacant"
+    elsif data['zoning'] == "MIXED"      
+      data['zoning'] = "Mixed"
+    elsif data['zoning'] == "RETAIL/ENT"      
+      data['zoning'] = "Commercial"      
+    elsif data['zoning'] == "VISITOR"      
+      data['zoning'] = "Commercial"      
+    elsif data['zoning'] == "RESIDENT"      
+      data['zoning'] = "Residential"           
+    elsif data['zoning'] == "MISSING DATA"      
+      data['zoning'] = "Vacant"  
+    elsif data['zoning'] == "CIE"      
+      data['zoning'] = "Commercial"          
+    elsif data['zoning'] == "MIXRES"      
+      data['zoning'] = "Mixed"
+    elsif data['zoning'] == "MIPS"      
+      data['zoning'] = "Mixed"      
+    end
+    
+    errors.concat( infer_geometry(data) )
+    errors.concat( infer_space_type(data) )
+    
+    errors.concat( super(data, schema) )
+    return errors
+  end
+
+  def clean_taxlot(data, schema)
+    errors = []
+
+    if data['type'] == 'taxlot'
+      data['type'] = 'Taxlot'
+    end
+    
+    if data['source_id'].class == Fixnum
+      data['source_id'] = data['source_id'].to_s
+    end
+    
+    if data['census_tract'].class == Fixnum
+      data['census_tract'] = data['census_tract'].to_s
+    end
+    
+    if data['zoning'] == "VACANT"      
+      data['zoning'] = "Vacant"
+    elsif data['zoning'] == "MIXED"      
+      data['zoning'] = "Mixed"
+    elsif data['zoning'] == "RETAIL/ENT"      
+      data['zoning'] = "Commercial"      
+    elsif data['zoning'] == "VISITOR"      
+      data['zoning'] = "Commercial"      
+    elsif data['zoning'] == "RESIDENT"      
+      data['zoning'] = "Residential"  
+    elsif data['zoning'] == "MISSING DATA"      
+      data['zoning'] = "Vacant"
+    elsif data['zoning'] == "CIE"      
+      data['zoning'] = "Commercial" 
+    elsif data['zoning'] == "MIXRES"      
+      data['zoning'] = "Mixed"      
+    elsif data['zoning'] == "MIPS"      
+      data['zoning'] = "Mixed"      
+    end   
+    
+    if data['zoning_source'] == "NREL_GDS"      
+      data['zoning_source'] = nil
+    end  
+    
+    errors.concat( super(data, schema) )
+    return errors
+  end
+  
+  def clean_region(data, schema)
+    errors = []
+
+    if data['type'] == 'region'
+      data['type'] = 'Region'
+    end
+    
+    if data['source_id'].class == Fixnum
+      data['source_id'] = data['source_id'].to_s
+    end
+    
+    errors.concat( super(data, schema) )
+    return errors
+  end
+
+
+  
+end
+
+cleaner = SanFranciscoCleaner.new 
+cleaner.clean
