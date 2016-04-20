afa9abd21e95eea498015f3bcd6a85718d90adcd
Dan Macumber <dan.macumber@gmail.com>
2016-04-20 09:35:29 -0600

    Updating run.rb to have a custom adapter

--- run.rb
+++ run.rb
@@ -1,15 +1,92 @@
 require 'openstudio-workflow'
+require 'rest-client'
 
 osw_path = ARGV[0]
 osw_dir = File.dirname(osw_path)
 
-# Create local adapters
-adapter_options = {workflow_filename: File.basename(osw_path), output_directory: File.join(osw_dir, 'run')}
-input_adapter = OpenStudio::Workflow.load_input_adapter 'local', adapter_options
-output_adapter = OpenStudio::Workflow.load_output_adapter 'local', adapter_options
+city_db_url = ARGV[1]
+
+datapoint_id = ARGV[2]
+
+# Create adapters
+input_options = {workflow_filename: File.basename(osw_path)}
+input_adapter = OpenStudio::Workflow.load_input_adapter('local', input_options)
+
+# DLM - have to do this first to get it to load the OutputAdapters class, won't actually use this object
+output_options = {output_directory: File.join(osw_dir, 'run'), url: city_db_url, datapoint_id: datapoint_id}
+output_adapter = OpenStudio::Workflow.load_output_adapter('local', output_options)
+
+# Custom Output Adapter
+module OpenStudio
+  module Workflow
+    module OutputAdapter
+      class CityDB < OutputAdapters
+        def initialize(options = {})
+          fail 'The required :output_directory option was not passed to the local output adapter' unless options[:output_directory]
+          super
+        end
+
+        # Write that the process has started
+        def communicate_started
+          File.open("#{@options[:output_directory]}/started.job", 'w') { |f| f << "Started Workflow #{::Time.now}" }
+          if @options[:url] && @options[:datapoint_id]
+            puts "communicate_started #{@options[:url]} #{@options[:datapoint_id]}"
+          end
+        end
+
+        # Write that the process has completed
+        def communicate_complete
+          File.open("#{@options[:output_directory]}/finished.job", 'w') { |f| f << "Finished Workflow #{::Time.now}" }
+          if @options[:url] && @options[:datapoint_id]
+            puts "communicate_started #{@options[:url]} #{@options[:datapoint_id]}"
+          end
+        end
+
+        # Write that the process has failed
+        def communicate_failure
+          File.open("#{@options[:output_directory]}/failed.job", 'w') { |f| f << "Failed Workflow #{::Time.now}" }
+          if @options[:url] && @options[:datapoint_id]
+            puts "communicate_started #{@options[:url]} #{@options[:datapoint_id]}"
+          end
+        end
+
+        # Do nothing on a state transition
+        def communicate_transition(_=nil, _=nil) end
+
+        # Write the measure attributes to the filesystem
+        def communicate_measure_attributes(measure_attributes, _=nil)
+          File.open("#{@options[:output_directory]}/measure_attributes.json", 'w') do |f|
+            f << JSON.pretty_generate(measure_attributes)
+          end
+        end
+
+        # Write the objective function results to the filesystem
+        def communicate_objective_function(objectives, _=nil)
+          obj_fun_file = "#{@options[:output_directory]}/objectives.json"
+          FileUtils.rm_f(obj_fun_file) if File.exist?(obj_fun_file)
+          File.open(obj_fun_file, 'w') { |f| f << JSON.pretty_generate(objectives) }
+        end
+
+        # Write the results of the workflow to the filesystem
+        def communicate_results(directory, results)
+          zip_results(directory)
+
+          if results.is_a? Hash
+            File.open("#{@options[:output_directory]}/data_point_out.json", 'w') { |f| f << JSON.pretty_generate(results) }
+          else
+            puts "Unknown datapoint result type. Please handle #{results.class}"
+          end
+        end
+      end
+    end
+  end
+end
+
+output_options = {output_directory: File.join(osw_dir, 'run'), url: city_db_url, datapoint_id: datapoint_id}
+output_adapter = OpenStudio::Workflow::OutputAdapter::CityDB.new(output_options)
 
 # Run workflow.osw
 run_options = Hash.new
 
-k = OpenStudio::Workflow::Run.new input_adapter, output_adapter, osw_dir, run_options
+k = OpenStudio::Workflow::Run.new(input_adapter, output_adapter, osw_dir, run_options)
 final_state = k.run
