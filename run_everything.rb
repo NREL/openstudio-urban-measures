6f592c3129a1a4adddf36c73fbe4171eba8de3ca
Dan Macumber <dan.macumber@gmail.com>
2016-04-19 21:19:12 -0600

    Making progress on run_everything.rb

--- run_everything.rb
+++ run_everything.rb
@@ -1,77 +1,139 @@
-def get_all_building_ids()
-  return []
-end
+require 'rest-client'
+require 'json'
 
-def get_all_workflow_ids()
-  return []
-end
+class Runner
 
-def get_datapoint(building_id, workflow_id)
-  return {}
-end
-
-def get_workflow(datapoint_id)
-  return {}
-end
-
-def run(workflow)
-
-end
-
-def post_datapoint_success(workflow)
-
-end
-
-def post_datapoint_failed(workflow)
-
-end
-
-# connect to database, get list of all building and workflow ids
-all_building_ids = get_all_building_ids
-all_workflow_ids = get_all_workflow_ids
-
-# loop over all combinations
-all_building_ids.each do |building_id|
-  all_workflow_ids.each do |workflow_id|
+  def initialize
+    @url = 'http://localhost:3000'
+    @project_id = '570d6b12c44c8d1e3800030b'
+    @user_name = 'test@nrel.gov'
+    @user_pwd = 'testing123'
+  end
     
-    # get data point for each pair of building_id, workflow_id
-    # data point is created if it doesn't already exist
-    datapoint = get_datapoint(building_id, workflow_id)
+  def get_all_building_ids()
+    result = []
     
-    # check if this already has dencity results or is queued to run
-    if !datapoint[:dencity_id].nil? || datapoint[:status] == "Queued"
-      next
+    json_request = JSON.generate('types' => ['Building'], 'project_id' => @project_id)
+    request = RestClient::Resource.new("#{@url}/api/export", user: @user_name, password: @user_pwd)
+    response = request.post(json_request, content_type: :json, accept: :json)
+    
+    buildings = JSON.parse(response.body, :symbolize_names => true)
+    buildings[:features].each do |building|
+      result << building[:properties][:id]
     end
-    
-    # datapoint is not run, get the workflow
-    # this is the merged workflow with the building properties merged in to the template workflow
-    workflow = get_workflow(datapoint_id)
 
-    # save workflow
-    File.open("./runs/#{workflow[:id]}.osw", 'w') do |file|
-      file << workflow
-    end
+    return result
+  end
+
+  def get_all_workflow_ids()
+    result = []
     
+    request = RestClient::Resource.new("#{@url}/workflows.json", user: @user_name, password: @user_pwd)
+    response = request.get(content_type: :json, accept: :json)
+  
+    workflows = JSON.parse(response.body, :symbolize_names => true)
+    workflows.each do |workflow|
+      result << workflow[:id]
+    end
+  
+    return result
+  end
+
+  def get_datapoint(building_id, workflow_id)
+    json_request = JSON.generate('workflow_id' => workflow_id, 'building_id' => building_id, 'project_id' => @project_id)
+    request = RestClient::Resource.new("#{@url}/api/retrieve_datapoint", user: @user_name, password: @user_pwd)
+    response = request.post(json_request, content_type: :json, accept: :json)
+    
+    datapoint = JSON.parse(response.body, :symbolize_names => true)
+    return datapoint[:datapoint]
+  end
+
+  def get_workflow(datapoint_id)
+    puts "#{@url}/datapoints/#{datapoint_id}/instance_workflow.json"
+    
+    request = RestClient::Resource.new("#{@url}/datapoints/#{datapoint_id}/instance_workflow.json", user: @user_name, password: @user_pwd)
+    response = request.get(content_type: :json, accept: :json)
+    
+    puts response.body
+    fail "hi"
+    workflow = JSON.parse(response.body, :symbolize_names => true)
+    return workflow
+  end
+
+  def run(workflow)
+
+  end
+
+  def post_datapoint_success(workflow)
+
+  end
+
+  def post_datapoint_failed(workflow)
+
+  end
+  
+  def create_osws
+    # connect to database, get list of all building and workflow ids
+    all_building_ids = get_all_building_ids
+    all_workflow_ids = get_all_workflow_ids
+
+    # loop over all combinations
+    all_building_ids.each do |building_id|
+      all_workflow_ids.each do |workflow_id|
+        
+        # get data point for each pair of building_id, workflow_id
+        # data point is created if it doesn't already exist
+        datapoint = get_datapoint(building_id, workflow_id)
+        datapoint_id = datapoint[:id]
+        
+        # check if this already has dencity results or is queued to run
+        if !datapoint[:dencity_id].nil? || datapoint[:status] == "Queued"
+          next
+        end
+        
+        # datapoint is not run, get the workflow
+        # this is the merged workflow with the building properties merged in to the template workflow
+        workflow = get_workflow(datapoint_id)
+
+        # save workflow
+        osw_dir = File.join(File.dirname(__FILE__), "/run/datapoint_#{datapoint_id}/")
+        FileUtils.rm_rf(osw_dir)
+        FileUtils.mkdir_p(osw_dir)
+
+        osw_path = "#{osw_dir}/in.osw"
+        File.open(osw_path, 'w') do |file|
+          file << workflow
+        end
+        
+      end
+    end
+  end
+  
+  def run_osws
+    Dir.glob("./runs/*.osw").each do |osw_path|
+
+      workflow = JSON::load(osw_path)
+
+      begin
+        # run the osw
+        run(workflow)
+        
+        dencity_id = nil
+        
+        # things worked, post back to the database that this datapoint is done and point to dencity id
+        post_datapoint_success(workflow, dencity_id)
+      rescue
+      
+        # things broke, post back to the database that this datapoint failed
+        post_datapoint_failed(workflow)
+      end
+      
+    end
   end
 end
 
+runner = Runner.new
+runner.create_osws
 
-Dir.glob("./runs/*.osw").each do |osw_path|
 
-  workflow = JSON::load(osw_path)
 
-  begin
-    # run the osw
-    run(workflow)
-    
-    dencity_id = nil
-    
-    # things worked, post back to the database that this datapoint is done and point to dencity id
-    post_datapoint_success(workflow, dencity_id)
-  rescue
-  
-    # things broke, post back to the database that this datapoint failed
-    post_datapoint_failed(workflow)
-  end
-  
-end
