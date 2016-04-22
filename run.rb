010eb561b7bef8fb63e9547f45acdfb8a18e8429
Dan Macumber <dan.macumber@gmail.com>
2016-04-22 16:55:19 -0600

    Trying to add dencity measures

--- run.rb
+++ run.rb
@@ -14,91 +14,95 @@ project_id = ARGV[3]
 input_options = {workflow_filename: File.basename(osw_path)}
 input_adapter = OpenStudio::Workflow.load_input_adapter('local', input_options)
 
-# DLM - have to do this first to get it to load the OutputAdapters class, won't actually use this object
-tmp_options = {output_directory: File.join(osw_dir, 'run'), url: city_db_url, datapoint_id: datapoint_id}
-tmp_adapter = OpenStudio::Workflow.load_output_adapter('local', tmp_options)
+# DLM - have to do this first to get it to load the OutputAdapters class
+output_options = {output_directory: File.join(osw_dir, 'run')}
+output_adapter = OpenStudio::Workflow.load_output_adapter('local', output_options)
 
-# Custom Output Adapter
-module OpenStudio
-  module Workflow
-    module OutputAdapter
-      class CityDB < OutputAdapters
-        def initialize(options = {})
-          fail 'The required :output_directory option was not passed to the local output adapter' unless options[:output_directory]
-          @user_name = 'test@nrel.gov'
-          @user_pwd = 'testing123'
-          super
-        end
-        
-        def send_status(status)
+if city_db_url && datapoint_id && project_id
 
-          datapoint = {}
-          datapoint[:id] = @options[:datapoint_id]
-          datapoint[:status] = status
-
-          params = {}
-          params[:project_id] = @options[:project_id]
-          params[:datapoint] = datapoint
-
-          request = RestClient::Resource.new('http://localhost:3000/api/datapoint', user: @user_name, password: @user_pwd)
-          response = request.post(params, content_type: :json, accept: :json)
-        end
-
-        # Write that the process has started
-        def communicate_started
-          File.open("#{@options[:output_directory]}/started.job", 'w') { |f| f << "Started Workflow #{::Time.now} #{@options}" }
-          fail 'Missing required options' unless @options[:url] && @options[:datapoint_id] && @options[:project_id]
-          send_status("Started")
-        end
-
-        # Write that the process has completed
-        def communicate_complete
-          File.open("#{@options[:output_directory]}/finished.job", 'w') { |f| f << "Finished Workflow #{::Time.now} #{@options}" }
-          fail 'Missing required options' unless @options[:url] && @options[:datapoint_id] && @options[:project_id]
-          send_status("Complete")
-        end
-
-        # Write that the process has failed
-        def communicate_failure
-          File.open("#{@options[:output_directory]}/failed.job", 'w') { |f| f << "Failed Workflow #{::Time.now} #{@options}" }
-          fail 'Missing required options' unless @options[:url] && @options[:datapoint_id] && @options[:project_id]
-          send_status("Failed")
-        end
-
-        # Do nothing on a state transition
-        def communicate_transition(_=nil, _=nil) end
-
-        # Write the measure attributes to the filesystem
-        def communicate_measure_attributes(measure_attributes, _=nil)
-          File.open("#{@options[:output_directory]}/measure_attributes.json", 'w') do |f|
-            f << JSON.pretty_generate(measure_attributes)
+  # Custom Output Adapter
+  module OpenStudio
+    module Workflow
+      module OutputAdapter
+        class CityDB < OutputAdapters
+          def initialize(options = {})
+            fail 'The required :output_directory option was not passed to the local output adapter' unless options[:output_directory]
+            @user_name = 'test@nrel.gov'
+            @user_pwd = 'testing123'
+            super
           end
-        end
+          
+          def send_status(status)
 
-        # Write the objective function results to the filesystem
-        def communicate_objective_function(objectives, _=nil)
-          obj_fun_file = "#{@options[:output_directory]}/objectives.json"
-          FileUtils.rm_f(obj_fun_file) if File.exist?(obj_fun_file)
-          File.open(obj_fun_file, 'w') { |f| f << JSON.pretty_generate(objectives) }
-        end
+            datapoint = {}
+            datapoint[:id] = @options[:datapoint_id]
+            datapoint[:status] = status
 
-        # Write the results of the workflow to the filesystem
-        def communicate_results(directory, results)
-          zip_results(directory)
+            params = {}
+            params[:project_id] = @options[:project_id]
+            params[:datapoint] = datapoint
 
-          if results.is_a? Hash
-            File.open("#{@options[:output_directory]}/data_point_out.json", 'w') { |f| f << JSON.pretty_generate(results) }
-          else
-            puts "Unknown datapoint result type. Please handle #{results.class}"
+            request = RestClient::Resource.new('http://localhost:3000/api/datapoint', user: @user_name, password: @user_pwd)
+            response = request.post(params, content_type: :json, accept: :json)
+          end
+
+          # Write that the process has started
+          def communicate_started
+            File.open("#{@options[:output_directory]}/started.job", 'w') { |f| f << "Started Workflow #{::Time.now} #{@options}" }
+            fail 'Missing required options' unless @options[:url] && @options[:datapoint_id] && @options[:project_id]
+            send_status("Started")
+          end
+
+          # Write that the process has completed
+          def communicate_complete
+            File.open("#{@options[:output_directory]}/finished.job", 'w') { |f| f << "Finished Workflow #{::Time.now} #{@options}" }
+            fail 'Missing required options' unless @options[:url] && @options[:datapoint_id] && @options[:project_id]
+            send_status("Complete")
+          end
+
+          # Write that the process has failed
+          def communicate_failure
+            File.open("#{@options[:output_directory]}/failed.job", 'w') { |f| f << "Failed Workflow #{::Time.now} #{@options}" }
+            fail 'Missing required options' unless @options[:url] && @options[:datapoint_id] && @options[:project_id]
+            send_status("Failed")
+          end
+
+          # Do nothing on a state transition
+          def communicate_transition(_=nil, _=nil) end
+
+          # Write the measure attributes to the filesystem
+          def communicate_measure_attributes(measure_attributes, _=nil)
+            File.open("#{@options[:output_directory]}/measure_attributes.json", 'w') do |f|
+              f << JSON.pretty_generate(measure_attributes)
+            end
+          end
+
+          # Write the objective function results to the filesystem
+          def communicate_objective_function(objectives, _=nil)
+            obj_fun_file = "#{@options[:output_directory]}/objectives.json"
+            FileUtils.rm_f(obj_fun_file) if File.exist?(obj_fun_file)
+            File.open(obj_fun_file, 'w') { |f| f << JSON.pretty_generate(objectives) }
+          end
+
+          # Write the results of the workflow to the filesystem
+          def communicate_results(directory, results)
+            zip_results(directory)
+
+            if results.is_a? Hash
+              File.open("#{@options[:output_directory]}/data_point_out.json", 'w') { |f| f << JSON.pretty_generate(results) }
+            else
+              puts "Unknown datapoint result type. Please handle #{results.class}"
+            end
           end
         end
       end
     end
   end
-end
 
-output_options = {output_directory: File.join(osw_dir, 'run'), url: city_db_url, datapoint_id: datapoint_id, project_id: project_id}
-output_adapter = OpenStudio::Workflow::OutputAdapter::CityDB.new(output_options)
+  output_options = {output_directory: File.join(osw_dir, 'run'), url: city_db_url, datapoint_id: datapoint_id, project_id: project_id}
+  output_adapter = OpenStudio::Workflow::OutputAdapter::CityDB.new(output_options)
+
+end
 
 # Run workflow.osw
 run_options = Hash.new
