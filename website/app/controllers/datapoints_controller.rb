8430bf4df01452266b45be83d04ec66d0e9c4dbc
Dan Macumber <dan.macumber@gmail.com>
2016-04-25 11:32:38 -0600

    Initial workflow runs, does not write to dencity

--- website/app/controllers/datapoints_controller.rb
+++ website/app/controllers/datapoints_controller.rb
@@ -18,11 +18,8 @@ class DatapointsController < ApplicationController
   # GET /datapoints/new
   def new
     @datapoint = Datapoint.new
-    logger.info("PARAMS: #{params}")
     @datapoint.building = params[:building]
-    logger.info("hey! #{@datapoint.building}")
     @datapoint.project = @datapoint.building.project
-    logger.info("hey2! #{@datapoint.project}")
     @workflows = get_workflows
 
   end
@@ -171,7 +168,7 @@ class DatapointsController < ApplicationController
             end
             
             if name == 'building_id'.to_sym
-              argument[:value] = region_hash[:id]
+              argument[:value] = building_hash[:id]
             elsif name == 'building_name'.to_sym
               argument[:value] = building_hash[:name]               
             end
@@ -181,12 +178,15 @@ class DatapointsController < ApplicationController
               #puts "Setting '#{name}' to '#{value}' based on building level properties" 
               argument[:value] = value
             end
+            
+            if name == 'datapoint_id'.to_sym
+              argument[:value] = @datapoint.id.to_s         
+            end
           end
         end
       end
     end
     
-    # DLM: how to get result into the json return?    
     respond_to do |format|
       if !error
         format.html { render json: result }
