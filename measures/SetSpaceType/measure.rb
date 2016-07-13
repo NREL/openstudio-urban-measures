d4e6a5c9866231a1622c4d8fab643869be29b270
Dan Macumber <dan.macumber@gmail.com>
2016-07-13 13:20:26 -0600

    Updating measures

--- measures/SetSpaceType/measure.rb
+++ measures/SetSpaceType/measure.rb
@@ -8,21 +8,25 @@
 # http://openstudio.nrel.gov/sites/openstudio.nrel.gov/files/nv_data/cpp_documentation_it/model/html/namespaces.html
 
 #start the measure
-class SetSpaceType < OpenStudio::Ruleset::ModelUserScript
+class SetBuildingType < OpenStudio::Ruleset::ModelUserScript
   
   #define the name that a user will see, this method may be deprecated as
   #the display name in PAT comes from the name field in measure.xml
   def name
-    return "Set Space Type"
+    return "Set Building Type"
   end
 
   #define the arguments that the user will input
   def arguments(model)
     args = OpenStudio::Ruleset::OSArgumentVector.new
     
-    space_type = OpenStudio::Ruleset::OSArgument::makeStringArgument("space_type",true)
-    space_type.setDisplayName("Space Type")
-    args << space_type
+    building_type = OpenStudio::Ruleset::OSArgument::makeStringArgument("building_type",true)
+    building_type.setDisplayName("Building Type")
+    args << building_type
+    
+    mixed_types = OpenStudio::Ruleset::OSArgument::makeStringArgument("mixed_types",false)
+    mixed_types.setDisplayName("Mixed Types")
+    args << mixed_types
     
     number_of_residential_units = OpenStudio::Ruleset::OSArgument::makeIntegerArgument("number_of_residential_units",true)
     number_of_residential_units.setDisplayName("Number of Residential Units")
@@ -42,29 +46,44 @@ class SetSpaceType < OpenStudio::Ruleset::ModelUserScript
     end
 
     #assign the user inputs to variables
-    space_type_name = runner.getStringArgumentValue("space_type",user_arguments)
+    building_type_name = runner.getStringArgumentValue("building_type",user_arguments)
+    mixed_types = runner.getStringArgumentValue("mixed_types",user_arguments)
     number_of_residential_units = runner.getIntegerArgumentValue("number_of_residential_units",user_arguments)
     
-    space_type = nil
+    if building_type_name == "Mixed use"
+      if mixed_types
+        mixed_types = JSON::parse(mixed_types, :symbolize_names=>true)
+      else
+        runner.registerError("'Mixed use' building type requested but 'mixed_types' argument is empty")
+        return false
+      end
+    else
+      if mixed_types
+        runner.registerWarning("Building type is 'building_type_name' and 'mixed_types' argument is not empty, input ignored")
+      end
+      mixed_types = []
+    end
+    
+    building_type = nil
     model.getSpaceTypes.each do |s|
-      if s.name.get == space_type_name
-        space_type = s
+      if s.name.get == building_type_name
+        building_type = s
         break
       end
     end
     
-    if space_type.nil?
-      space_type = OpenStudio::Model::SpaceType.new(model)
-      space_type.setName(space_type_name)
-      space_type.setStandardsBuildingType(space_type_name)
-      space_type.setStandardsSpaceType(space_type_name)
+    if building_type.nil?
+      building_type = OpenStudio::Model::SpaceType.new(model)
+      building_type.setName(building_type_name)
+      building_type.setStandardsBuildingType(building_type_name)
+      building_type.setStandardsSpaceType(building_type_name)
     end
     
-    model.getBuilding.setSpaceType(space_type)
-    model.getBuilding.setStandardsBuildingType(space_type_name)
+    model.getBuilding.setSpaceType(building_type)
+    model.getBuilding.setStandardsBuildingType(building_type_name)
     model.getBuilding.setStandardsNumberOfLivingUnits(number_of_residential_units)
     
-    if space_type_name == "Mobile Home"  
+    if building_type_name == "Mobile Home"  
       model.getBuilding.setRelocatable(true)
     else
       model.getBuilding.setRelocatable(false)
@@ -77,4 +96,4 @@ class SetSpaceType < OpenStudio::Ruleset::ModelUserScript
 end #end the measure
 
 #this allows the measure to be use by the application
-SetSpaceType.new.registerWithApplication
+SetBuildingType.new.registerWithApplication
