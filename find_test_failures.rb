efce8c32e571e7d78f7dc8216164c4104be4874e
Dan Macumber <dan.macumber@gmail.com>
2017-05-08 09:37:29 -0600

    Update to find test failures

--- find_test_failures.rb
+++ find_test_failures.rb
@@ -1,31 +1,29 @@
 require 'json'
 
-dirs = Dir.glob("./run/testing*")
+dirs = Dir.glob("./run/testing/*")
 
 euis = []
 failed = []
 num_success = 0
 num_failed = 0
 dirs.each do |osw_dir|
-  ['baseline', 'retrofit'].each do |workflow|
-    if File.exists?(File.join(osw_dir, workflow, "run/failed.job"))
-      failed << "#{osw_dir}/#{workflow} failed to run"
-      num_failed += 1
-    else
-      num_success += 1
-    end
-    
-    if File.exists?(File.join(osw_dir, workflow, "out.osw"))
-      File.open(File.join(osw_dir, workflow, "out.osw"), 'r') do |f|
-        out_osw = JSON::parse(f.read, :symbolize_names => true)
-        if out_osw[:steps]
-          out_osw[:steps].each do |step|
-            if step[:measure_dir_name] == 'openstudio_results'
-              if step[:result] && step[:result][:step_values]
-                step[:result][:step_values].each do |result|
-                  if result[:name] == 'eui'
-                    euis << "#{osw_dir}/#{workflow} EUI = #{result[:value]}"
-                  end
+  if File.exists?(File.join(osw_dir, "run/failed.job"))
+    failed << "#{osw_dir} failed to run"
+    num_failed += 1
+  else
+    num_success += 1
+  end
+  
+  if File.exists?(File.join(osw_dir, "out.osw"))
+    File.open(File.join(osw_dir, "out.osw"), 'r') do |f|
+      out_osw = JSON::parse(f.read, :symbolize_names => true)
+      if out_osw[:steps]
+        out_osw[:steps].each do |step|
+          if step[:measure_dir_name] == 'openstudio_results'
+            if step[:result] && step[:result][:step_values]
+              step[:result][:step_values].each do |result|
+                if result[:name] == 'eui'
+                  euis << "#{osw_dir} EUI = #{result[:value]}"
                 end
               end
             end
@@ -34,6 +32,7 @@ dirs.each do |osw_dir|
       end
     end
   end
+
 end
 
 euis.each {|eui| puts eui}
