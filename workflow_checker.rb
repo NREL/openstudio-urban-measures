20479ef50e42325e0a7dd9aaac1f23d5cb0a3255
Rob Guglielmetti <rob.guglielmetti@gmail.com>
2018-04-06 16:50:20 -0600

    add report

--- workflow_checker.rb
+++ workflow_checker.rb
@@ -6,11 +6,27 @@ file = File.read filename
 wf = JSON.parse(file)
 puts "Workflow integrity check (#{wf['steps'].size} steps):"
 mt = ""
+mtype = 0
+wc = 0
 wf['steps'].each_with_index do |step, i|
+    warn = ''
     step['measure_definition']['attributes'].each do |a|
         if a['name'] == "Measure Type"
+            if a['value'] == "ModelMeasure" && mtype > 0 || a['value'] == "EnergyPlusMeasure" && mtype > 1
+                warn = '**'
+                wc += 1
+            end
+
             mt = a['value']
-        end
+            if a['value'] == "EnergyPlusMeasure"
+                mtype = 1
+            elsif a['value'] == "ReportingMeasure"
+                mtype = 2
+            end
+         end
     end
-    puts "STEP #{i} (#{mt}): #{step['name']}"
+    puts "#{warn}STEP #{i} (#{mt}): #{step['name']}"
+end
+if wc > 0
+    puts "WARNING: #{wc} workflow steps are out of type order (**)."
 end
