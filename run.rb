ad4a65cca4c7a31925c50138eefd4f836440d2e5
kflemin <katherine.fleming@nrel.gov>
2018-09-12 16:43:07 -0600

    workaround for running with OS 2.6.1 and openstudio-standards 0.2.3.  fixes sizing run issue when called from urbanopt measures

--- run.rb
+++ run.rb
@@ -17,17 +17,36 @@ require 'openstudio'
 require 'openstudio-workflow'
 require 'openstudio/workflow/adapters/output_adapter'
 
+
+# Log lever
 $logger = Logger.new(STDOUT)
-$logger.level = Logger::ERROR
+#$logger.level = Logger::ERROR
 #$logger.level = Logger::WARN
-#$logger.level = Logger::DEBUG
+$logger.level = Logger::DEBUG
 
 OpenStudio::Logger.instance.standardOutLogger.enable
-OpenStudio::Logger.instance.standardOutLogger.setLogLevel(OpenStudio::Error)
-#OpenStudio::Logger.instance.standardOutLogger.setLogLevel(OpenStudio::Warn)
+#OpenStudio::Logger.instance.standardOutLogger.setLogLevel(OpenStudio::Error)
+OpenStudio::Logger.instance.standardOutLogger.setLogLevel(OpenStudio::Warn)
 #OpenStudio::Logger.instance.standardOutLogger.setLogLevel(OpenStudio::Debug)
 
 debug = true
+
+
+# **START** 
+# Set up all bundler paths to require appropriate gems (needed)
+# Then blank out bundler paths for subsequent OpenStudio CLI calls 
+# This is a work-around for the CLI call inside OpenStudio-standards gem
+# when measures invoke sizing runs
+require 'bundler'
+Bundler.setup
+Bundler.require
+
+ENV["BUNDLE_PATH"] = nil
+ENV["BUNDLE_GEMFILE"] = nil
+
+# **END** 
+
+
 
 osw_path = ARGV[1]
 osw_dir = File.dirname(osw_path)
